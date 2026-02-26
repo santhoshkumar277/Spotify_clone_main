d85e1e383002c32eaf302f375829676da2f0dc55
santhoshkumar277 <113783226+santhoshkumar277@users.noreply.github.com>
2026-02-26 19:38:13 +0530

    Serve dashboard.html instead of index.html

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.post('/login', (req, res) => {
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'dashboard.html'));
 });
 
 const PORT = 3000;
