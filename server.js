0531293887e4443d737698e564ebce62858f0e67
santhoshkumar277 <113783226+santhoshkumar277@users.noreply.github.com>
2026-02-27 19:34:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -71,3 +71,6 @@ const PORT = process.env.PORT || 10000;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
