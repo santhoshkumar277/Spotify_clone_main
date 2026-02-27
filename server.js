c5ce9a7c51e283308776663cc38dcad1e52dbf02
santhoshkumar277 <113783226+santhoshkumar277@users.noreply.github.com>
2026-02-27 19:17:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -62,7 +62,12 @@ app.post('/login', (req, res) => {
 app.get('/dashboard', (req, res) => {
     res.sendFile(path.join(__dirname, 'dashboard.html'));
 });
-
+/*
 app.listen(3000, () => {
     console.log('Server running at http://localhost:3000');
+});
+*/
+const PORT = process.env.PORT || 10000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
